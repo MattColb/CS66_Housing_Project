73bba1cd2df565eba4b0bb0e47f1153743caf7c4
MattColb <matt@4colberts.com>
2022-11-26 10:14:16 -0600

    Update dbwork.py

--- dbwork.py
+++ dbwork.py
@@ -19,18 +19,28 @@ def state_averages(state, year, rooms):
             for col in temp.columns:
                 if len(col) >7 and col[6:8] == ("_" + rooms):
                     return int(temp[col].mean())
-def medians_main():
-    for db in range(len(list_median)):
-        for col in list_median[db][0].columns:
-            if not(col[4:6] == "50" or col[0:3] == "pop" or col == "state_alpha"  or col[0:2] == "hu" or col.lower() == "countyname"):
-                list_median[db][0].drop(col, inplace=True, axis=1)
 
-    states_dictionary = {}
-    for state in list_median[0][0].state_alpha.unique():
-        states_dictionary[state] = {}
-        for year in list_median:
-            states_dictionary[state][year[1]] = {}
-            for room in range(5):
-                states_dictionary[state][year[1]][str(room)] = state_averages(state, year[1], str(room))
+for db in range(len(list_median)):
+    for col in list_median[db][0].columns:
+        if not(col[4:6] == "50" or col[0:3] == "pop" or col == "state_alpha"  or col[0:2] == "hu" or col.lower() == "countyname"):
+            list_median[db][0].drop(col, inplace=True, axis=1)
 
-medians_main()
+states_dictionary = {}
+
+for state in list_median[0][0].state_alpha.unique():
+    states_dictionary[state] = {}
+    for year in list_median:
+        states_dictionary[state][year[1]] = {}
+        for room in range(5):
+            states_dictionary[state][year[1]][str(room)] = state_averages(state, year[1], str(room))
+
+def average_by_state(state):
+    beds = [[],[],[],[],[]]
+    years = states_dictionary[state].keys()
+    for i in years:
+        for rooms in range(5):
+            beds[rooms].append(states_dictionary[state][i][str(rooms)])
+    print(beds, years)
+
+
+average_by_state("AL")
