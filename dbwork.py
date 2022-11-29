e27a13f95ad5e0f2d4a88e8b71a61e757063e294
MattColb <matt@4colberts.com>
2022-11-29 10:36:27 -0600

    Called pandas as pd in the dbwork file

--- dbwork.py
+++ dbwork.py
@@ -1,14 +1,14 @@
-import pandas
+import pandas as pd
 import os
 
 list_median = []
 
 for file in os.listdir("./FY2001-2018-50-percentile/"):
-    list_median.append([pandas.read_excel("./FY2001-2018-50-percentile/" +file), str(file[2:6])])
+    list_median.append([pd.read_excel("./FY2001-2018-50-percentile/" +file), str(file[2:6])])
 
-main_housing_data = pandas.read_csv("clean_2000_2018.csv")
+main_housing_data = pd.read_csv("clean_2000_2018.csv")
 
-homeless_data = pandas.read_csv("HSH_90_day_emergency_shelter_waitlist.csv")
+homeless_data = pd.read_csv("HSH_90_day_emergency_shelter_waitlist.csv")
 
 
 def state_averages(state, year, rooms):
@@ -25,22 +25,32 @@ for db in range(len(list_median)):
         if not(col[4:6] == "50" or col[0:3] == "pop" or col == "state_alpha"  or col[0:2] == "hu" or col.lower() == "countyname"):
             list_median[db][0].drop(col, inplace=True, axis=1)
 
-states_dictionary = {}
-
-for state in list_median[0][0].state_alpha.unique():
-    states_dictionary[state] = {}
+states_list = list_median[0][0].state_alpha.unique()
+pricing = []
+for state in range(len(states_list)):
+    pricing.append([states_list[state]])
     for year in list_median:
-        states_dictionary[state][year[1]] = {}
+        curr_prices = []
         for room in range(5):
-            states_dictionary[state][year[1]][str(room)] = state_averages(state, year[1], str(room))
+            curr_prices.append(state_averages(states_list[state], year[1], str(room)))
+        pricing[state].append(curr_prices)
 
-def average_by_state(state):
-    beds = [[],[],[],[],[]]
-    years = states_dictionary[state].keys()
-    for i in years:
-        for rooms in range(5):
-            beds[rooms].append(states_dictionary[state][i][str(rooms)])
-    print(beds, years)
+colnames=["States"]
+for year in list_median:
+    colnames.append(year[1])
+
+states_df = pd.DataFrame(pricing, columns=colnames)
+print(states_df)
+
+#states_df = pandas.DataFrame(columns=colnames)
+
+#def average_by_state(state):
+#    beds = [[],[],[],[],[]]
+#    years = states_dictionary[state].keys()
+#    for i in years:
+#        for rooms in range(5):
+#            beds[rooms].append(states_dictionary[state][i][str(rooms)])
+#    print(beds, years)
 
 
-average_by_state("AL")
+#average_by_state("AL")
